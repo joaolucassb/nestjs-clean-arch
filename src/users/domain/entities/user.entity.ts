3f230ebe91d9b228bc2f7c20faa5b5ce8889f6fa
João Lucas <97396678+joaolucassb@users.noreply.github.com>
2026-05-22 16:29:07 -0300

    UserEntity constructor unit tests

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -7,6 +7,6 @@ export type UserProps = {
 
 export class UserEntity {
   constructor(public readonly props: UserProps) {
-    this.props.createdAt = this.props.createdAt ??new Date();
+    this.props.createdAt = this.props.createdAt ?? new Date();
   }
 }
