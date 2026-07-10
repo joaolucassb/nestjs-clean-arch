ddea9967d173945dbf01fabc6a518c6b901843dd
João Lucas <97396678+joaolucassb@users.noreply.github.com>
2026-07-09 21:31:34 -0300

    Integration tests - validate password field on create user

--- src/users/domain/entities/__tests__/integration/user.entity.int.spec.ts
+++ src/users/domain/entities/__tests__/integration/user.entity.int.spec.ts
@@ -1,59 +1,84 @@
-import { UserDataBuilder } from "@/users/domain/testing/helpers/user-data-builder"
-import { UserEntity, UserProps } from "../../user.entity"
-import { EntityValidationError } from "@/shared/domain/errors/validation-error"
+import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder';
+import { UserEntity, UserProps } from '../../user.entity';
+import { EntityValidationError } from '@/shared/domain/errors/validation-error';
 
 describe('UserEntity integration tests', () => {
   describe('Constructor method', () => {
     it('Shuold throw an error when creating a user with invalid name', () => {
       let props: UserProps = {
         ...UserDataBuilder({}),
-        name: null as any
-      }
+        name: null as any,
+      };
       expect(() => new UserEntity(props)).toThrow(EntityValidationError);
 
       props = {
         ...UserDataBuilder({}),
-        name: ''
-      }
+        name: '',
+      };
       expect(() => new UserEntity(props)).toThrow(EntityValidationError);
 
       props = {
         ...UserDataBuilder({}),
-        name: 10 as any
-      }
+        name: 10 as any,
+      };
       expect(() => new UserEntity(props)).toThrow(EntityValidationError);
 
       props = {
         ...UserDataBuilder({}),
-        name: 'a'.repeat(256)
-      }
+        name: 'a'.repeat(256),
+      };
       expect(() => new UserEntity(props)).toThrow(EntityValidationError);
-    })
+    });
 
     it('Shuold throw an error when creating a user with invalid email', () => {
       let props: UserProps = {
         ...UserDataBuilder({}),
-        email: null as any
-      }
+        email: null as any,
+      };
       expect(() => new UserEntity(props)).toThrow(EntityValidationError);
 
       props = {
         ...UserDataBuilder({}),
-        email: ''
-      }
+        email: '',
+      };
       expect(() => new UserEntity(props)).toThrow(EntityValidationError);
 
       props = {
         ...UserDataBuilder({}),
-        email: 10 as any
-      }
+        email: 10 as any,
+      };
       expect(() => new UserEntity(props)).toThrow(EntityValidationError);
 
       props = {
         ...UserDataBuilder({}),
-        email: 'a'.repeat(256)
-      }
+        email: 'a'.repeat(256),
+      };
       expect(() => new UserEntity(props)).toThrow(EntityValidationError);
-    })
-  })
-})
+    });
+    it('Shuold throw an error when creating a user with invalid password', () => {
+      let props: UserProps = {
+        ...UserDataBuilder({}),
+        password: null as any,
+      };
+      expect(() => new UserEntity(props)).toThrow(EntityValidationError);
+
+      props = {
+        ...UserDataBuilder({}),
+        password: '',
+      };
+      expect(() => new UserEntity(props)).toThrow(EntityValidationError);
+
+      props = {
+        ...UserDataBuilder({}),
+        password: 10 as any,
+      };
+      expect(() => new UserEntity(props)).toThrow(EntityValidationError);
+
+      props = {
+        ...UserDataBuilder({}),
+        password: 'a'.repeat(101),
+      };
+      expect(() => new UserEntity(props)).toThrow(EntityValidationError);
+    });
+  });
+});
