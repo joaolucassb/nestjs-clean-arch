05db0dbcd4dfcb1a6ba2eb5247e7493d87bf2268
João Lucas <97396678+joaolucassb@users.noreply.github.com>
2026-07-16 22:43:56 -0300

    Adding SarchResult class

--- src/shared/domain/repositories/searchable-repository-contracts.ts
+++ src/shared/domain/repositories/searchable-repository-contracts.ts
@@ -4,68 +4,83 @@ import { RepositoryInterface } from './repository-contracts';
 export type SortDirection = 'asc' | 'desc';
 
 export type SearchProps<Filter = string> = {
-  page?: number
-  perPage?: number
-  sort?: string | null
-  sortDir?: SortDirection | null
-  filter?: Filter | null
-}
+  page?: number;
+  perPage?: number;
+  sort?: string | null;
+  sortDir?: SortDirection | null;
+  filter?: Filter | null;
+};
+
+export type SearchResultProps<E extends Entity, Filter> = {
+  items: E[];
+  total: number;
+  currentPage: number;
+  perPage: number;
+  sort: string | null;
+  sortDir: string | null;
+  filter: Filter | null;
+};
 
 export class SearchParams {
-  protected _page: number
-  protected _perPage: number = 15
-  protected _sort: string | null
-  protected _sortDir: SortDirection | null
-  protected _filter: string | null
+  protected _page: number;
+  protected _perPage: number = 15;
+  protected _sort: string | null;
+  protected _sortDir: SortDirection | null;
+  protected _filter: string | null;
 
   constructor(props: SearchProps = {}) {
-    this.page = props.page ?? 1
-    this.perPage = props.perPage ?? 15
-    this.sort = props.sort ?? null
-    this.sortDir = props.sortDir ?? null
-    this.filter = props.filter ?? null
+    this.page = props.page ?? 1;
+    this.perPage = props.perPage ?? 15;
+    this.sort = props.sort ?? null;
+    this.sortDir = props.sortDir ?? null;
+    this.filter = props.filter ?? null;
   }
 
   get page() {
-    return this._page
+    return this._page;
   }
 
   private set page(value: number) {
     let _page = +value;
     if (Number.isNaN(_page) || _page <= 0 || parseInt(_page as any) !== _page) {
-      _page = 1
+      _page = 1;
     }
     this._page = _page;
   }
 
   get perPage() {
-    return this._perPage
+    return this._perPage;
   }
 
   private set perPage(value: number) {
     let _perPage = value === (true as any) ? this._perPage : +value;
-    if (Number.isNaN(_perPage) || _perPage <= 0 || parseInt(_perPage as any) !== _perPage) {
+    if (
+      Number.isNaN(_perPage) ||
+      _perPage <= 0 ||
+      parseInt(_perPage as any) !== _perPage
+    ) {
       _perPage = this._perPage;
     }
     this._perPage = _perPage;
   }
 
   get sort() {
-    return this._sort
+    return this._sort;
   }
 
   private set sort(value: string | null) {
-    this._sort = value === null || value === undefined || value === '' ? null : `${value}`;
+    this._sort =
+      value === null || value === undefined || value === '' ? null : `${value}`;
   }
 
   get sortDir() {
-    return this._sortDir
+    return this._sortDir;
   }
 
   private set sortDir(value: string | null) {
-    if(!this._sort) {
+    if (!this._sort) {
       this._sortDir = null;
-      return
+      return;
     }
 
     const dir = `${value}`.toLowerCase();
@@ -73,11 +88,47 @@ export class SearchParams {
   }
 
   get filter() {
-    return this._filter
+    return this._filter;
   }
 
   private set filter(value: string | null) {
-    this._filter = value === null || value === undefined || value === '' ? null : `${value}`;
+    this._filter =
+      value === null || value === undefined || value === '' ? null : `${value}`;
+  }
+}
+
+export class SarchResult<E extends Entity, Filter = string> {
+  readonly items: E[];
+  readonly total: number;
+  readonly currentPage: number;
+  readonly perPage: number;
+  readonly lastPage: number;
+  readonly sort: string | null;
+  readonly sortDir: string | null;
+  readonly filter: Filter | null;
+
+  constructor(props: SearchResultProps<E, Filter>) {
+    this.items = props.items;
+    this.total = props.total;
+    this.currentPage = props.currentPage;
+    this.perPage = props.perPage;
+    this.lastPage = Math.ceil(this.total / this.perPage);
+    this.sort = props.sort ?? null;
+    this.sortDir = props.sortDir ?? null;
+    this.filter = props.filter ?? null;
+  }
+
+  toJSON(forceEntity = false) {
+    return {
+      items: forceEntity ? this.items.map(item => item.toJSON()) : this.items,
+      total: this.total,
+      currentPage: this.currentPage,
+      perPage: this.perPage,
+      lastPage: this.lastPage,
+      sort: this.sort,
+      sortDir: this.sortDir,
+      filter: this.filter,
+    };
   }
 }
 
