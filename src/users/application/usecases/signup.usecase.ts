3c0fbc27d7ee21599a690988251891199a0ce8a3
João Lucas <97396678+joaolucassb@users.noreply.github.com>
2026-08-02 22:30:55 -0300

    Adding UpdateUserUseCase

--- src/users/application/usecases/signup.usecase.ts
+++ src/users/application/usecases/signup.usecase.ts
@@ -2,7 +2,7 @@ import { UserRepository } from '@/users/domain/repositories/user.repository';
 import { BadRequestError } from '../../../shared/application/errors/bad-request-error';
 import { UserEntity } from '@/users/domain/entities/user.entity';
 import { HashProvider } from '@/shared/application/providers/hash-provider';
-import { UserOutput } from '../dto/user-output';
+import { UserOutput, UserOutputMapper } from '../dto/user-output';
 import { UseCase as DefaultUseCase } from '@/shared/application/usecases/use-case';
 
 export namespace SignUpUseCase {
@@ -31,7 +31,7 @@ export namespace SignUpUseCase {
         Object.assign(input, { password: hashPassword }),
       );
       await this.userRepository.insert(entity);
-      return entity.toJSON();
+      return UserOutputMapper.toOutput(entity);
     }
   }
 }
